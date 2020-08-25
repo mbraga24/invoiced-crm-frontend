7edaff83b1fdad3c509c0a6106179de43e597107
github username <rdg.brg@gmail.com>
2020-08-25 17:52:46 -0400

    Add contacts on click

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 import Contact from './Contact';
 import data from './data';
 
-const renderContacts = () => {
-  return data.map(info => (
-    <Contact key={info.id} { ...info } />
-  ))
-}
 
 class App extends Component {
+  state = {
+    contacts: data
+  }
+
+  renderContacts = () => {
+    return this.state.contacts.map(info => (
+      <Contact key={info.id} { ...info } />
+    ))
+  }
+
+  handleClick = e => {
+    e.preventDefault()
+    const contacts = this.state.contacts
+    const newId = contacts[contacts.length - 1].id + 1
+    this.setState({
+      contacts: [...this.state.contacts, { id: newId, name: `Name-${newId}`, email: `name-${newId}@example.com` } ]
+    })
+  }
+  
   render() {
     return (
-      <div className="pure-g">
-        {renderContacts()}
+      <div id="App">
+        <a href="#" className="pure-button" onClick={this.handleClick}>Add Contact</a>
+        <div className="pure-g">
+          {this.renderContacts()}
+        </div>
       </div>
     );
   }
