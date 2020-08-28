09a14282d4fea96576103356653c258ad91018aa
github username <rdg.brg@gmail.com>
2020-08-27 22:21:43 -0400

    Change Collection to functional component

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -3,25 +3,22 @@ import Contact from './Contact';
 import AddContactForm from './AddContactForm';
 import './Collection.css';
 
-class Collection extends Component {
+const Collection = props => {
 
-  renderContacts = () => {
-    return this.props.contacts.map(info => (
+  const renderContacts = () => {
+    return props.contacts.map(info => (
       <Contact key={info.id} { ...info } />
     ))
   }
 
-  render() {
-    console.log(this.props.contacts)
-    return (
-      <div id="Collection" className="pure-container">
-        <AddContactForm addContact={this.props.addContact}/>
-        <div className="pure-g">
-          {this.renderContacts()}
-        </div>
+  return (
+    <div id="Collection" className="pure-container">
+      <AddContactForm addContact={props.addContact}/>
+      <div className="pure-g">
+        {renderContacts()}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Collection;
