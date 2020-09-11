276e0e1fda339003966b29828b44b3c13a9ca644
github username <rdg.brg@gmail.com>
2020-09-10 20:17:53 -0400

    Import Provider coomponent and store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'purecss/build/pure.css';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './components/App.js';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(
-  <Router> 
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router> 
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
