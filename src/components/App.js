8c57957672c25442de584fbf77efa0fadb2f575e
github username <rdg.brg@gmail.com>
2020-09-11 14:49:12 -0400

    App useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import data from './data';
+import { useSelector } from 'react-redux';
 import Collection from './Collection';
 import Show from './Show';
 import styles from './App.module.sass';
 
 const App = () => {
 
-  const [ contacts, setContacts ] = useState([])
-
   useEffect(() => {
-    setContacts(data)
+    // setContacts(data)
+
   }, [])
+
+  const contacts = useSelector(state => state.contacts)
 
   const addContact = newContactInfo => {
     console.log("New Contact Info:", newContactInfo)
     const newId = contacts[contacts.length - 1].id + 1
     console.log(newId)
-    setContacts([...contacts, { id: newId, name: newContactInfo.name , email: newContactInfo.email } ])
+    // setContacts([...contacts, { id: newId, name: newContactInfo.name , email: newContactInfo.email } ])
   }
-
+  
+  // console.log("APP COMP:", contacts)
   return (
     <div id="App" className={styles.main}>
       <Switch>
-        <Route exact path="/contacts" render={() => <Collection addContact={addContact} contacts={contacts}/>} />
+        <Route exact path="/contacts" render={() => <Collection addContact={addContact} />} />
         <Route path="/contacts/:contactId" component={Show} />
         <Redirect from="/" to="/contacts" />
       </Switch>
